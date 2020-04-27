1c19a2f7f14d6bc4ba5d07301280a88d130f0d3b
Piotr Kaminski <piterwk1@.wp.pl>
2020-04-27 23:58:21 +0200

    add button

--- src/ui/base-element.js
+++ src/ui/base-element.js
@@ -1,12 +1,29 @@
 import $ from 'jquery';
 
 export class BaseElement {
+  constructor() {
+    this.element = null; // jquery element
+  }
 
-    constructor() {
-        this.element = null;  //jquery element
-    }
+  appendToElement(el) {
+    this.createElement();
+    el.append(this.element);
+  }
 
-    getElementString() {
-        
-    }
+  createElement() {
+    const s = this.getElementString();
+    this.element = $(s);
+    this.enableJS();
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  getElementString() {
+    // eslint-disable-next-line no-throw-literal
+    throw 'Please override getElementString() in BaseElement';
+    // throw new Error('Please override getElementString() in BaseElement');
+  }
+
+  enableJS() {
+    componentHandler.upgradeElement(this.element[0]);
+  }
 }
