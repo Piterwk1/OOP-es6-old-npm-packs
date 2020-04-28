da8cf5d2512797dec8f0aec77441ffdc982e5e34
Piotr Kaminski <piterwk1@.wp.pl>
2020-04-28 08:24:29 +0200

    add image

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ import { Drone } from './classes/drone.js';
 import { fleet } from './fleet-data.js';
 import { FleetDataService } from './services/fleet-data-service.js';
 import { Button } from './ui/button.js';
+import { Image } from './ui/image.js';
 
 const b = new Button('Click Me');
 b.appendToElement($('body'));
+
+const i = new Image('../images/drone.jpg');
+i.appendToElement($('body'));
